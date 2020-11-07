d7f4a28fa08f55cd6b90042a19f58b84c67580fc
Haibo Wang <haibo.wang2@student.unsw.edu.au>
2020-11-07 20:14:02 +1100

    hhh

--- src/control.erl
+++ src/control.erl
@@ -1,12 +1,12 @@
 -module(control).
--export([graphToNetwork/0]).
-circularNetwork3 () ->
-  [{red  , [{white, [white, blue]}]},
-   {white, [{blue , [red, blue]}]},
-   {blue , [{red  , [red, white]}]}
-  ].
-graphToNetwork() ->
-    Graph = circularNetwork3(),
+-export([graphToNetwork/1, extendNetwork/4]).
+% circularNetwork3 () ->
+%   [{red  , [{white, [white, blue]}]},
+%    {white, [{blue , [red, blue]}]},
+%    {blue , [{red  , [red, white]}]}
+%   ].
+graphToNetwork(Graph) ->
+    % Graph = circularNetwork3(),
     Routers = ets:new(undef, [private]),
     ETable = ets:new(undef, [private]),
     % below code is for spwan routers
@@ -53,9 +53,6 @@ loop(Expected) ->
         {abort, _, 0} -> false
     end.
 
-    % List = ets:match_object(ETable, {'$0', '$1'}),
-    % io:format("Router ~p~n", [List]).
-
 initRouters(Graph, Routers, ETable) ->
     lists:foreach(fun({NodeName, Edges}) -> 
         [{_, N}] =  ets:lookup(ETable, NodeName),
@@ -68,6 +65,7 @@ initRouters(Graph, Routers, ETable) ->
                 end}
         end, Graph).
 
+
 initRoutingTable([], _, RT) ->
     RT;
 initRoutingTable([{Nei, Labels}|T], Routers, RT) ->
@@ -76,5 +74,58 @@ initRoutingTable([{Nei, Labels}|T], Routers, RT) ->
     initRoutingTable(T, Routers, RT ++ R).
 
 
+initRoutingTableByid([], RT) ->
+    RT;
+initRoutingTableByid([{Nid, Labels}|T], RT) ->
+    R = [{Dst, Nid} || Dst <- Labels],
+    initRoutingTableByid(T, RT ++ R).
 
-%% extendNetwork(RootPid, SeqNum, From, {NodeName, Edges}) ->
+spawnFun(Table1, NodeName, NodeList, Cpid) ->
+    Pid = router:start(NodeName),
+    Pid ! {control, self(), Cpid, 0, 
+        fun(_, Table2)->
+            ets:insert(Table2, NodeList),
+            ets:insert(Table2, {'$NoInEdges', 1})
+        end},
+    ets:insert(Table1, {NodeName, Pid}),
+    Pid.
+
+extendNetwork(RootPid, SeqNum, From, {NodeName, Edges}) ->
+    NewNodeList = initRoutingTableByid(Edges, []),
+    Cpid = self(),
+    RootPid ! {control, self(), self(), SeqNum, 
+        fun(Name, Table) ->
+            case Name of
+                From -> % spawn new process
+                    try spawnFun(Table, NodeName, NewNodeList, Cpid) of
+                        Pid ->[Pid]
+                    catch
+                        _:_ -> abort
+                    end;
+                _ ->
+                    % if the node can get to 'From' then can get to the new node
+                    Next = ets:lookup(Table, From),
+                    case Next of
+                        [{_, Nid}] ->
+                            ets:insert(Table, {NodeName, Nid})
+                    end,
+                    % if a node has connection with the new node then update $NoInEdges
+                    lists:foreach(
+                        fun({In, _}) -> 
+                            case self() of
+                                In ->  
+                                    [{_, N}] = ets:lookup(Table, '$NoInEdges'),
+                                    ets:insert(Table, {'$NoInEdges', N+1})
+                                end
+                        
+                        end, Edges),
+                    [] % no processed spawned
+            end
+        end},
+    receive
+        {committed, _, SeqNum} ->
+            true;
+        {abort, _, SeqNum} ->
+            false
+    end.
+    
--- src/router.erl
+++ src/router.erl
@@ -6,25 +6,191 @@ start(RouterName) ->
 
 init(RouterName) ->
     Rtable = ets:new(undef, [private]),
-    Ctable = ets:new(undef, [private]),
-    % ets:insert(Rtable, {'$NoInEdges', 0}).
-    loop(RouterName, Rtable, Ctable).
+    loop(RouterName, Rtable, []).
 
-loop(RouterName, Rtable, Ctable) ->
+loop(RouterName, Rtable, Seqtable) ->
    receive
-       {control, From, Pid, SeqNum, ControlFun} ->
-           NRtable = handleControl(RouterName, Rtable, Ctable, From, Pid, SeqNum, ControlFun),
-           loop(RouterName, NRtable, Ctable)
-        
-        end.
-
-handleControl(RouterName, Rtable, _, _, Pid, 0, ControlFun) ->
-    case ControlFun(RouterName, Rtable) of
-        abort ->
-            Pid ! {abort, self(), 0},
-            exit(abort);
+        {message, Dest, From, Pid, Trace} ->
+            NewTrace = [RouterName|Trace],
+            case RouterName of
+                Dest -> 
+                    Pid ! {trace, self(), lists:reverse(NewTrace)};
+                _ ->
+                    Next = ets:lookup(Rtable, Dest),
+                    % TODO change here
+                    case Next of
+                        [] -> io:format("something bad happend~n");
+                        [{_, Nid}] -> 
+                            Nid ! {message, Dest, self(), Pid, NewTrace}
+                    end
+            end,
+            loop(RouterName, Rtable, Seqtable);
+        {control, From, Pid, SeqNum, ControlFun} when SeqNum == 0 ->
+            case ControlFun(RouterName, Rtable) of
+                abort ->
+                    Pid ! {abort, self(), 0},
+                    exit(abort);
+                _ -> 
+                    Pid ! {committed, self(), 0},
+                    loop(RouterName, Rtable, Seqtable)
+            end;
+        {control, From, Pid, SeqNum, ControlFun} when SeqNum /= 0 ->
+           NRtable = handleControl(RouterName, Rtable, Seqtable, From, Pid, SeqNum, ControlFun),
+           loop(RouterName, NRtable, Seqtable);
+        {dump, From} ->
+            Dump = ets:match(Rtable, '$1'),
+            From ! {table, self(), Dump},
+            loop(RouterName, Rtable, Seqtable);
+        stop ->
+            ets:delete(Rtable),
+            clearMailbox(),
+            exit(stop);
         _ -> 
-            Pid ! {committed, self(), 0},
-            Rtable
+            loop(RouterName, Rtable, Seqtable) 
         end.
 
+clearMailbox() ->
+    receive
+        _Any ->
+            clearMailbox()
+        after 0 ->
+            ok
+        end.
+
+handleControl(RouterName, Rtable, Seqtable, From, Pid, SeqNum, ControlFun) ->
+    NewSeq = [SeqNum|Seqtable],
+    Dump = lists:flatten(ets:match(Rtable, '$1')),
+    TempTable = ets:new(undef, [private]),
+    ets:insert(TempTable, Dump),
+    case Pid of
+        % this is root node 
+        From -> 
+            broadcast(Rtable, {control, self(), Pid, SeqNum, ControlFun}),
+            Res = handleControlFun(RouterName, TempTable, ControlFun),
+            Replys = coordinate1(Rtable, SeqNum, NewSeq),
+            case Res of
+                abort ->
+                    % here broadcast phase1 result to everyone
+                    broadcast(Rtable, {doAbort, self(), SeqNum}),
+                    Pid ! {abort, self(), SeqNum},
+                    ets:delete(TempTable),
+                    Rtable;
+                Children when Replys == abort ->
+                    broadcast(Rtable, {doAbort, self(), SeqNum}),
+                    lists:foreach(fun(C) -> 
+                            exit(C, abort)
+                        end, Children),
+                    Pid ! {abort, self(), SeqNum},
+                    ets:delete(TempTable),
+                    Rtable;
+                Children when Replys == yes ->
+                    broadcast(Rtable, {doCommit, self(), SeqNum}),
+                    Pid ! {committed, self(), SeqNum},
+                    ets:delete(Rtable),
+                    TempTable
+            end;
+        % normal nodes
+        _Else ->
+            broadcast(Rtable, {control, self(), Pid, SeqNum, ControlFun}),
+            case handleControlFun(RouterName, TempTable, ControlFun) of
+                abort ->
+                    % phase1
+                    coordinate1(Rtable, SeqNum, NewSeq),
+                    From ! {abort, self(), SeqNum},
+                    % phase2 
+                    % Replys is not important here
+                    coordinate2(SeqNum),
+                    broadcast(Rtable, {doAbort, self(), SeqNum}),
+                    ets:delete(TempTable),
+                    Rtable;
+                Children ->
+                    Reply1 = coordinate1(Rtable, SeqNum, NewSeq),
+                    case Reply1 of
+                        abort -> From ! {abort, self(), SeqNum};
+                        yes -> From ! {yes, self(), SeqNum}
+                    end,
+                    Reply2 = coordinate2(SeqNum),
+                    broadcast(Rtable, {Reply2, self(), SeqNum}),
+                    case Reply2 of
+                        doAbort ->
+                            ets:delete(TempTable),
+                            lists:foreach(fun(C) -> 
+                                    exit(C, abort)
+                                end, Children),
+                            Rtable;
+                        doCommit ->
+                            ets:delete(Rtable),
+                            TempTable
+                    end
+            end
+    end.
+
+coordinate2(SeqNum) ->
+    receive 
+        {doAbort, From, SeqNum} ->
+            doAbort;
+        {doCommit, From, SeqNum} ->
+            doCommit;
+        _ -> coordinate2(SeqNum)
+    end.
+
+coordinate1(Rtable, SeqNum, Seqtable) ->
+    Neis = [Nei || [{Dst, Nei}] <- ets:match(Rtable, '$1'), Dst =/= '$NoInEdges'] ,
+    NodeCount = length(lists:usort(Neis)),
+    phase1Loop(SeqNum, Seqtable, NodeCount).
+
+phase1Loop(SeqNum, Seqtable, NodeCount) ->
+    receive
+        {abort, _, SeqNum} -> 
+            % consume msg, wait until timeout to abort
+            case NodeCount - 1 of
+                0 -> abort;
+                _ -> phase1Loop(SeqNum, Seqtable, NodeCount - 1)
+            end;
+        {yes, _, SeqNum} -> 
+            case NodeCount - 1 of
+                0 -> yes;
+                _ -> phase1Loop(SeqNum, Seqtable, NodeCount - 1)
+            end;
+        {control, From, _, SeqNum, _} ->
+            From ! {yes, self(), SeqNum},
+            phase1Loop(SeqNum, Seqtable, NodeCount);
+        % abort both control msg
+        {control, From, _, SeqNum1, _} ->
+            Doesfind = lists:member(SeqNum1, Seqtable),
+            case Doesfind of
+                true -> doNothing;
+                _Else -> 
+                    From ! {abort, self(), SeqNum1}
+            end,
+            abort;
+        % abort both control msg
+        {_, From, SeqNum1} -> 
+            Doesfind = lists:member(SeqNum1, Seqtable),
+            case Doesfind of
+                true -> doNothing;
+                _Else -> 
+                    From ! {abort, self(), SeqNum1}
+            end,
+            abort
+    after timeout() -> abort
+    end.
+
+handleControlFun(RouterName, Rtable, ControlFun) ->
+    Children = ControlFun(RouterName, Rtable),
+    case Children of
+        abort ->
+            abort;
+        _ ->
+            Children
+    end.
+
+broadcast(Rtable, Msg) ->
+    Neis = [Nei || [{Dst, Nei}] <- ets:match(Rtable, '$1'), Dst =/= '$NoInEdges'] ,
+    UniNeis = lists:usort(Neis),
+    lists:foreach(
+        fun(Nid) ->
+            Nid ! Msg
+        end, UniNeis).
+
+timeout () -> 5000.
